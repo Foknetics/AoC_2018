946901cc073f6f3d6ff5294497524d76ebf88055
Connor Bratten <connorabratten@gmail.com>
2018-12-01 21:21:02 -0500

    Add day 1 solutions

---
+++ day1-1.py
@@ -0,0 +1,19 @@
+with open('day1_input.txt') as file:
+    data = file.read()
+
+frequency_changes = data.splitlines()
+
+frequency = 0
+possible_matches = []
+for frequency_change in frequency_changes:
+    frequency += int(frequency_change)
+    possible_matches.append(frequency)
+
+match_found = False
+while not match_found:
+    for frequency_change in frequency_changes:
+        frequency += int(frequency_change)
+        if frequency in possible_matches:
+            print('First frequency repeat:', frequency)
+            match_found = True
+            break
---
+++ day1.py
@@ -0,0 +1,7 @@
+with open('day1_input.txt') as file:
+    data = file.read()
+
+frequency = 0
+for frequency_change in data.splitlines():
+    frequency += int(frequency_change)
+print('Ending frequency:', frequency)
