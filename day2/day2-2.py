73010524cbadd2b2660598b2e8b2274a6c7cc509
Connor Bratten <connorabratten@gmail.com>
2018-12-04 22:18:15 -0500

    Format output properly for day 2 solution

--- day2/day2-2.py
+++ day2/day2-2.py
@@ -15,9 +15,15 @@ def similar_id(id_1, id_2):
 
 boxes = data.splitlines()
 
+found = False
 for box in boxes:
     for other_box in boxes:
         if other_box == box:
             continue
         elif similar_id(box, other_box):
-            print(box, other_box)
+            found = True
+            break
+    if found:
+        break
+
+print('The common letters between the two correct boxes are:', ''.join([x for x in box if x in other_box]))
